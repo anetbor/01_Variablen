ef75cf1df43aa7e2d49cb2bbfb6a1159eedda4b6
Anet <anetborin@mail.com>
2022-01-17 09:22:06 +0100

    variablen

--- script_01.js
+++ script_01.js
@@ -4,4 +4,13 @@
 
 
 
+// console.log("Hello World!");
+// console.log(firstName); //nicht deklarierte Var. -->Fehler
 
+let firstName; // Deklaration/definition
+firstName = "Max";// Wertzuweisung
+console.log(firstName);//Ausgabe
+
+let familyName ="Mütze"; //Deklaration und Wertzuweisung zusammen
+console.log(familyName);
+console.log(firstName + " " + familyName);

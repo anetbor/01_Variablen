5cbaaee6f50ff84770baf41fde9e1bcb47887572
Anet <anetborin@mail.com>
2022-01-17 11:07:18 +0100

    17.01.2022

--- script_01.js
+++ script_01.js
@@ -7,10 +7,45 @@
 // console.log("Hello World!");
 // console.log(firstName); //nicht deklarierte Var. -->Fehler
 
-let firstName; // Deklaration/definition
-firstName = "Max";// Wertzuweisung
-console.log(firstName);//Ausgabe
+// let firstName; // Deklaration/definition
+// firstName = "Max";// Wertzuweisung
+// console.log(firstName);//Ausgabe
 
-let familyName ="Mütze"; //Deklaration und Wertzuweisung zusammen
-console.log(familyName);
-console.log(firstName + " " + familyName);
+// let familyName ="Mütze"; //Deklaration und Wertzuweisung zusammen
+// console.log(familyName);
+// console.log(firstName + " " + familyName);
+
+// let firstName,familyName;
+// firstName = prompt("Bitte Vorname eingeben:");
+// familyName = prompt("Bitte Nachname eingeben:");
+// console.log(firstName + " " + familyName);
+
+// let test;
+// test = "Hi";
+// test = 2;
+// test = true;
+// console. log("Datentyp:" + typeof test)
+// console. log("Inhalt:" + test)
+
+// const test = "hi";
+// test = 2;
+// console. log("Inhalt:" + test);
+
+let ageJohn, ageMark;
+let birthYearJohn, birthYearMark;
+
+// let year = 2022;
+let date = new Date();
+let year = date.getFullYear();
+console.log("Date():" + year);
+
+ageJohn = 25;
+ageMark = 30;
+// Berechnung
+birthYearJohn = year - ageJohn;
+birthYearMark = year - ageMark;
+
+//Ausgabe
+
+console.log("birthYearJohn:" + birthYearJohn);
+console.log("birthYearMark:" + birthYearMark);
